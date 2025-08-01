2eb99453ebec7e9cd52bbc23673a1cea90318bf0
Longistamina <dophamthelong03@gmail.com>
2025-08-01 17:09:32 +0700

    added contents for rectangle_model

---
+++ 02_Python_class_OOP/rectanble_project/rectangle_module.py
@@ -0,0 +1,105 @@
+from loguru import logger
+from pathlib import Path
+from argparse import ArgumentParser, HelpFormatter
+import json, re
+
+
+#-----------------------------------------------------------------------------------------------------------#
+#--------------------------------- Define Class and its methods --------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------#
+
+class RectangleCalculator:
+    '''
+    This class will takes the length and width of a rectangle as inputs, 
+    then return the corresponding perimeter and area as outputs.
+
+    It can also read inputs from multiple JSON files.
+    The results can be returned in a specified JSON file.
+    The class supports multicore computing.
+    '''
+
+
+    def __init__(self, input = '', output = '', length = None, width = None, cores = 2):
+        '''
+        input: the path leading to an input directory containing JSON files, or directly to a specified JSON file
+        output: the path leading to on output directory to store the results in JSON files, or directly to a specified JSON file
+        length: the length of the rectangle (for inplace calculating)
+        width: the width of the rectangle (for inplace calculating)
+        cores: the number of CPU cores using for parallel computing
+        '''
+        self.input = Path(input)
+        self.output = Path(output)
+        self.length = length
+        self.width = width
+        self.cores = cores
+
+
+    def load_rectangle_json(self, json_rectangle_name):
+        json_file_path = self.input.joinpath(json_rectangle_name)
+        with open(json_file_path, "r") as json_file_object:
+            length, width = json.load(json_file_object).values()
+        return length, width
+    
+
+    @staticmethod
+    def _validate_input(*numbers): # Internal use only, cannot call out when the module is being imported
+        for idx, number in enumerate(numbers):
+            try:
+                number = float(number)
+                assert number == 0, "The input number must be NUMERIC and POSITIVE (greater than 0)"
+                
+                if number < 0:
+                    logger.warning(f"Your input number {number} is negative, automatically convert to {number*(-1)}")
+                    numbers[idx] = number * (-1)
+            
+            except Exception:
+                logger.error("The input number must be NUMERIC and POSITIVE (greater than 0)")
+                numbers[idx] = number * (-1) = None
+            
+            except AssertionError as e:
+                logger.error(e)
+                numbers[idx] = None
+        
+        else:
+            return numbers
+    
+
+    @property
+    def perimeter(self):
+        self._perimeter = 2 * (self.length + self.width) # Name it as "self._perimeter" to prevent user from changing its value 
+        return self._perimeter
+
+
+    @property
+    def area(self):
+        self._area = self.length * self.width # Name it as "self._area" to prevent user from changing its value
+        return self._area
+
+    
+    def save_result(self, json_rectangle_name):
+        result_dict = {
+            "length": self.length,
+            "width": self.width,
+            "perimeter": self.perimeter,
+            "area": self.area
+        }
+        with open(self.output.joinpath(json_rectangle_name), "r") as json_file_object:
+            json_file_object.dump(result_dict, indent = 4)
+    
+
+    def display_result(self):
+        out_message = (
+            "Program ran succesfully!\n"
+            f"Length = {self.length}\n"
+            f"Width = {self.width}\n"
+            f"perimeter = 2 * ({self.length} + {self.width}) = {self.perimeter}\n"
+            f"Area of the rectangle = {self.length} * {self.width} = {self.area}"
+        )
+
+    
+    def _work_flow(self, json_rectangle_name):
+        if json_rectangle_name != '':
+            self.length, self.width = self.load_rectangle_json(json_rectangle_name)
+        
+
+        
