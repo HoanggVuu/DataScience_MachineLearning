1a36090e18f35a5e1cfccaeb9f4e5312b6b3c4e3
Longistamina <dophamthelong03@gmail.com>
2025-08-02 13:03:54 +0700

    added content for rectangle project

--- 02_Python_class_OOP/rectanble_project/rectangle_module.py
+++ 02_Python_class_OOP/rectanble_project/rectangle_module.py
@@ -2,7 +2,15 @@ from loguru import logger
 from pathlib import Path
 from argparse import ArgumentParser, HelpFormatter
 import json, re
+from termcolor import colored
+import multiprocessing
+from shutil import rmtree
 
+# Add file sink while keeping the default console output
+logger.add("02_Python_class_OOP/rectangle_project/rectangle_logs.txt", 
+           rotation="1 MB",  # Rotate when file reaches 1MB
+           retention="10 days",  # Keep logs for 10 days
+           level="WARNING")
 
 #-----------------------------------------------------------------------------------------------------------#
 #--------------------------------- Define Class and its methods --------------------------------------------#
@@ -29,45 +37,66 @@ class RectangleCalculator:
         '''
         self.input = Path(input)
         self.output = Path(output)
-        self.length = length
-        self.width = width
+        self.__length = length
+        self.__width = width
         self.cores = cores
 
+        match str(self.output):
+            case "":
+                pass
+            case _:
+                if self.output.exists():
+                    rmtree(self.output)
+                    self.output.mkdir()
 
-    def load_rectangle_json(self, json_rectangle_name):
-        json_file_path = self.input.joinpath(json_rectangle_name)
-        with open(json_file_path, "r") as json_pointer:
-            length, width = json.load(json_pointer).values()
-        return length, width
+                elif (self.output.suffix != "") and (self.output.parent.exists()):
+                    self.output = self.output.stem.mkdir(exist_ok = True)
+                    logger.warning(f"Your output path should be a directory, not a file, automatically set as {self.output}")
+
+                elif self.output.parent.exists():
+                    self.output.mkdir()
+
+                elif not self.output.parent.exists():
+                    self.output = Path.cwd().joinpath("result")
+                    self.output.mkdir()
+                    logger.warning(f"Your output path's parent directory does not exists, automatically set as {self.output}")
+                else:
+                    pass
+
     
 
     @staticmethod
     def __validate_input(*numbers): # Internal use only, cannot call out when the module is being imported
+        numeric_pattern = r"^\d+\.?\d+$"
+        numbers = list(numbers)
         for idx, number in enumerate(numbers):
-            try:
-                number = float(number)
-                assert number == 0, "The input number must be NUMERIC and POSITIVE (greater than 0)"
-                
-                if number < 0:
-                    logger.warning(f"Your input number {number} is negative, automatically convert to {number*(-1)}")
-                    numbers[idx] = number * (-1)
-            
-            except Exception:
-                logger.error("The input number must be NUMERIC and POSITIVE (greater than 0)")
-                numbers[idx] = number * (-1) = None
-            
-            except AssertionError as e:
-                logger.error(e)
+            if re.match(numeric_pattern, str(number)):
+                numbers[idx] = float(number)
+            else:
                 numbers[idx] = None
         
+        return numbers
+
+
+    def load_rectangle_json(self, json_rectangle_name):
+        if len(Path(json_rectangle_name).parts) > 1:
+            json_file_path = json_rectangle_name
         else:
-            return numbers
+            json_file_path = self.input.joinpath(json_rectangle_name)
+        
+        with open(json_file_path, "r") as json_pointer:
+            length, width = json.load(json_pointer).values()
+            length, width = RectangleCalculator.__validate_input(length, width)
+        
+        if None in [length, width]:
+            logger.warning(f"The inputs in this {json_rectangle_name} file are corrupted!!!")
+        return length, width
     
 
     @property
     def perimeter(self):
         self.length, self.width = self.__validate_input(self.length, self.width)
-        if (self.length is None) or self.width is None:
+        if (self.length is None) or (self.width is None):
             self.__perimeter = None
         else:
             self.__perimeter = 2 * (self.length + self.width) # Name it as "self.__perimeter" to prevent user from changing its value 
@@ -79,7 +108,7 @@ class RectangleCalculator:
     def area(self):
         self.length, self.width = self.__validate_input(self.length, self.width)
 
-        if (self.length is None) or self.width is None:
+        if (self.length is None) or (self.width is None):
             self.__area = None
         else:
             self.__area = self.length * self.width # Name it as "self.__area" to prevent user from changing its value
@@ -93,24 +122,80 @@ class RectangleCalculator:
             "perimeter": self.perimeter,
             "area": self.area
         }
-        with open(self.output.joinpath(json_rectangle_name), "r") as json_pointer:
-            json_pointer.dump(result_dict, indent = 4)
+        
+        json_rectangle_name = Path(json_rectangle_name).name
+        if not json_rectangle_name.endswith(".json"):
+            json_rectangle_name += ".json"
+            logger.warning(f"The file does not end with '.json', automatically set as {json_rectangle_name}")
+        with open(self.output.joinpath(json_rectangle_name), "w") as json_pointer:
+            json.dump(result_dict, json_pointer, indent = 4)
     
 
-    def summary(self):
-        out_message = (
-            "Program ran succesfully!\n"
-            f"Length = {self.length}\n"
-            f"Width = {self.width}\n"
-            f"perimeter = 2 * ({self.length} + {self.width}) = {self.perimeter}\n"
-            f"area = {self.length} * {self.width} = {self.area}"
-        )
-        logger.info(out_message)
+    def summary(self, json_rectangle_name = "nameless"):
+        match str(self.output):
+            case "":
+                json_rectangle_name = colored(json_rectangle_name, "red", attrs = ["bold"])
+                out_message = (
+                    f"Result of {json_rectangle_name} rectangle:\n"
+                    f"++ Length = {self.length}\n"
+                    f"++ Width = {self.width}\n"
+                    f"++ Perimeter = 2 * ({self.length} + {self.width}) = {self.perimeter}\n"
+                    f"++ Area = {self.length} * {self.width} = {self.area}"
+                )
+                logger.info(out_message)
+            case _:
+                self.save_result(json_rectangle_name)
 
 
-    def _CommandLine_WorkFlow(self, json_rectangle_name):
+    def _SingleInput_WorkFlow(self, json_rectangle_name):
         if json_rectangle_name != '':
             self.length, self.width = self.load_rectangle_json(json_rectangle_name)
+            
+            if (None not in [self.__length, self.__width]):
+                logger.warning(f"Detected valid inputs in {json_rectangle_name}, prioritize them for calculation.")
+  
+            self.summary(json_rectangle_name)
         
+        elif None in [self.__length, self.__width]:
+            logger.error("No input was given")
+        else:
+            self.length, self.width = self.__length, self.__width
+            self.summary()
+            
 
+#------------------------------------------------------------------------------------------------------------#
+#------------------------------------------ Define main() function ------------------------------------------#
+#------------------------------------------------------------------------------------------------------------#
+
+def main():
+    try:
+        calculator = RectangleCalculator(
+            #input = "02_Python_class_OOP/rectangle_project/data/",
+            #output = "02_Python_class_OOP/rectangle_project/result",
+            length = 2,
+            width = 3,
+            cores = 4
+        )
+
+        if calculator.input.suffix == ".json":
+            calculator._SingleInput_WorkFlow(calculator.input)
         
+        elif calculator.input.is_dir():
+            input_files = [(entry.name,) for entry in calculator.input.glob("*.json") ]
+
+            with multiprocessing.Pool(processes = calculator.cores) as pool:
+                pool.starmap(func = calculator._SingleInput_WorkFlow, iterable = input_files)
+
+        else:
+            calculator.summary()
+    
+    except Exception as e:
+        logger.error(e)
+
+
+#---------------------------------------------------------------------------------------------------------#
+#------------------------------------------ Run main() function ------------------------------------------#
+#---------------------------------------------------------------------------------------------------------#
+
+if __name__ == "__main__":
+    main()
