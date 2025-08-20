3bb4ab39350078394afd1724d057767821b31c9c
Longistamina <dophamthelong03@gmail.com>
2025-08-20 13:42:13 +0700

    added miscelaneous methods file

---
+++ 03_Pandas_DataR_dataframe/01_Pandas_python_style/04_Series_apply_transform_agg_groupby_filter_where_pipe_combine_roll_expand_ewn.py
@@ -0,0 +1,15 @@
+'''
+Flow of contents:
+1. .apply(): Element-wise Function Application
+2. .map(): Mapping Function to Series
+3. .transform(): Group-wise Transformation
+4. .agg(): Aggregation Functions
+5. .groupby(): Grouping Data
+6. .filter(): Filtering Data Based on Conditions
+7. .where(): Conditional Replacement
+8. .pipe(): Method Chaining with Custom Functions
+9. .combine(): Combining Two Series
+10. .rolling(): Rolling Window Calculations
+11. .expanding(): Expanding Window Calculations
+12. .ewm(): Exponential Weighted Functions
+'''
